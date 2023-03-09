6606870b749a51794cc2e4c4a9afc38b4f42587e
luisma722 <josikooo69@gmail.com>
2023-03-09 16:53:29 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import Phaser from 'phaser'
-
-import HelloWorldScene from './HelloWorldScene'
+//import HelloWorldScene from './HelloWorldScene'
+import titulo from './titulo.js'
+import game from './game.js'
 
 const config = {
 	type: Phaser.AUTO,
 	parent: 'app',
-	width: 800,
-	height: 600,
+	width: 1024,
+	height: 1024,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 },
 		},
 	},
-	scene: [HelloWorldScene],
+	scene: [titulo, game],
 }
 
 export default new Phaser.Game(config)
