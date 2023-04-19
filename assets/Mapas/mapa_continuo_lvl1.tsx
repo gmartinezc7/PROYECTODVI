9a8b73165c9c9f57eb710388c775b08d26134c3d
WhiteMoon-Knight <60646885+WhiteMoon-Knight@users.noreply.github.com>
2023-04-19 12:31:56 +0200

    mapa tiled

---
+++ assets/Mapas/mapa_continuo_lvl1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="mapa_continuo_lvl1" tilewidth="72" tileheight="72" tilecount="1000" columns="10">
+ <image source="repo/PROYECTODVI/assets/mapa_continuo_lvl1.png" width="720" height="7200"/>
+</tileset>
